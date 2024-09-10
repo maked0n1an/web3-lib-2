fc2d8fcb87cbf51557af1f98ffb7dbea9f5aff16
maked0n1an <amedik733@gmail.com>
2024-09-10 09:03:12 +0300

    Replaced AutoRepr and Singleton to 'common' file in utils/

--- libs/async_eth_lib/models/others.py
+++ libs/async_eth_lib/models/others.py
@@ -7,26 +7,6 @@ from ..models.contract import (
     TokenContract
 )
 
-# region Common
-class AutoRepr:
-    """Contains a __repr__ function that automatically builds the output of a class using all its variables."""
-
-    def __repr__(self) -> str:
-        values = ('{}={!r}'.format(key, value)
-                  for key, value in vars(self).items())
-        return '{}({})'.format(self.__class__.__name__, ', '.join(values))
-
-
-class Singleton(type):
-    """A class that implements the singleton pattern."""
-    _instances = {}
-
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            cls._instances[cls] = super(
-                Singleton, cls).__call__(cls, *args, **kwargs)
-        return cls._instances[cls]
-# endregion Common
 
 # region Constants
 class TokenSymbol:
