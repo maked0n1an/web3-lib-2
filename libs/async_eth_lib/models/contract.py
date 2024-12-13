8de05396080ae786a18a2a94e4cc01e0559128d6
maked0n1an <promka4146@gmail.com>
2024-12-13 13:30:58 +0200

    async_eth_lib: renewed models/contract.py

--- libs/async_eth_lib/models/contract.py
+++ libs/async_eth_lib/models/contract.py
@@ -1,19 +1,22 @@
-from web3 import (
-    Web3,
-    types
+from web3 import Web3
+from web3.types import (
+    Address,
+    ChecksumAddress,
 )
+
+from libs.async_eth_lib.models.dataclasses import DefaultAbis
 
 from .common import AutoRepr
 
 
-#region RawContract
+# region RawContract
 class RawContract(AutoRepr):
     def __init__(
         self,
         title: str,
-        address: str | types.Address | types.ChecksumAddress | types.ENS,
-        abi_path: list[str] | tuple[str] | str | None
-    ) -> None:
+        address: str | Address | ChecksumAddress,
+        abi_path: list[str] | tuple[str] | str
+    ):
         """
         Initialize the class.
 
@@ -25,19 +28,18 @@ class RawContract(AutoRepr):
         self.title = title
         self.address = Web3.to_checksum_address(address)
         self.abi_path = abi_path
-#endregion RawContract
+# endregion RawContract
 
 
-#region TokenContract
+# region TokenContract
 class TokenContract(RawContract):
     def __init__(
-        self, 
+        self,
         title: str,
-        address: str | types.Address | types.ChecksumAddress | types.ENS, 
-        abi_path: list[str] | tuple[str] | str = None,
+        address: str | Address | ChecksumAddress,
+        abi_path: list[str] | tuple[str] | str = DefaultAbis.ERC_20,
         decimals: int | None = None,
-        is_native_token: bool = False
-    ) -> None:
+    ):
         """
         Initialize the class.
 
@@ -49,32 +51,29 @@ class TokenContract(RawContract):
             is_native_token (bool): is this contract native token of network (False).
         """
         super().__init__(
-            title = title,
+            title=title,
             address=address,
             abi_path=abi_path
         )
         self.decimals = decimals
-        self.is_native_token = is_native_token
-#endregion TokenContract
+        self.is_native_token = False
+# endregion TokenContract
 
 
-#region NativeTokenContract
-class NativeTokenContract(TokenContract):
+# region NativeTokenContract
+class NativeTokenContract(AutoRepr):
     """
     An instance of a native token contract.
 
     Attributes:
         title (str): The title or name of the native token.
-
     """
 
     def __init__(
         self,
-        title: str,
-        address: str = '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',
-        abi_path: list[str] | tuple[str] | str = None,
-        decimals: int = 18
-    ) -> None:
+        title: str = 'ETH',
+        address: str | Address | ChecksumAddress = '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',
+    ):
         """
         Initialize the NativeTokenContract.
 
@@ -82,11 +81,8 @@ class NativeTokenContract(TokenContract):
             title (str): The title or name of the native token.
 
         """
-        super().__init__(
-            title=title,
-            address=address,
-            abi_path=abi_path,
-            decimals=decimals,
-            is_native_token=True
-        )
-#endregion NativeTokenContract
+        self.title = title
+        self.address = Web3.to_checksum_address(address)
+        self.decimals = 18
+        self.is_native_token = True
+# endregion NativeTokenContract
