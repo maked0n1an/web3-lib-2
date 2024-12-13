d91b146ad5307d1fe533f7672c812b3c0ef26902
maked0n1an <promka4146@gmail.com>
2024-12-13 13:13:07 +0200

    async_eth_lib: decorators updated.

--- libs/async_eth_lib/utils/decorators.py
+++ libs/async_eth_lib/utils/decorators.py
@@ -4,41 +4,33 @@ import os
 from ..architecture.logger import console_logger
 from ..models.operation import OperationInfo
 
-def validate_swap_tokens(
+
+def validate_operation_tokens(
     available_tokens: list[str],
-    call_depth: str = 2
+    op_name: str,
+    class_name: str
 ):
     def decorator(func):
-        caller_frame = inspect.currentframe()
-        for _ in range(call_depth):
-            caller_frame = caller_frame.f_back
-        calling_line = f"{os.path.basename(caller_frame.f_code.co_filename)}:{caller_frame.f_lineno}"
-        
-        async def _wrapper(self, swap_info: OperationInfo, *args, **kwargs):
-            from_token = swap_info.from_token_name.upper()
-            to_token = swap_info.to_token_name.upper()
-            
+        async def _wrapper(self, operation_info: OperationInfo, *args, **kwargs):
+            from_token = operation_info.from_token_name
+            to_token = operation_info.to_token_name
+
             if (
                 from_token not in available_tokens
                 or
                 to_token not in available_tokens
             ):
-                message = (
-                    f'Not supported tokens to swap:'
+                console_logger.error(
+                    f'Not supported tokens to {op_name} in {class_name}:'
                     f' {from_token} or {to_token}'
                 )
-                console_logger.error(f"{calling_line:<15} | {message}")
                 return
-            
             if from_token == to_token:
-                message = (
-                    f'The tokens for swap() are equal:'
+                console_logger.error(
+                    f'The tokens for {op_name}() are equal:'
                     f' {from_token} == {to_token} '
                 )
-                console_logger.error(f"{calling_line:<15} | {message}")
                 return
-            
-            return await func(self, swap_info, *args, **kwargs)
-        
+            return await func(self, operation_info, *args, **kwargs)
         return _wrapper
     return decorator
